0d4c0712b3c1e92369a6d98c33cc1343b404ab98
Ryan Stevens <39173142+RyanAsherStevens@users.noreply.github.com>
2018-07-19 19:10:54 -0400

    Add files via upload
    Uploaded close to final version of html, css, and javascript

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /* JavaScript */
 
+// added a slick carousel to the pictures
 
 $(document).ready(function(){
-    console.log("hi");
     $('.pictures').slick({
     dots: true,
     infinite: true,
@@ -11,3 +11,13 @@ $(document).ready(function(){
     cssEase: 'linear'
     });
 });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
