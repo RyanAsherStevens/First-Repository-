73121e779eacba37ded2c7ceb204483e339831c2
Ryan Stevens <39173142+RyanAsherStevens@users.noreply.github.com>
2018-07-26 17:40:49 -0400

    Add files via upload

--- my_first_repository/main.js
+++ my_first_repository/main.js
@@ -1,1 +1,23 @@
-/* Java */
+/* JavaScript */
+
+// added a slick carousel to the pictures
+
+$(document).ready(function(){
+    $('.pictures').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+    });
+});
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
